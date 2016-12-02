f74349deb981f2fd090423e8ff8f83fc7833ac40
Lee Tsai <lee.x.tsai@gmail.com>
2016-12-02 15:45:11 -0800

    [Refactor] Updated to find the longest run of zeroes

--- getBase.js
+++ getBase.js
@@ -7,9 +7,27 @@ function getBase(number, base) {
     newN += remainder;
     number = Math.floor(number / base);
   }
-  return newN.split("").reverse().join("");
+
+  return findLongestZero(newN.split("").reverse().join(""));
 }
 
-getBase(10, 2); // 1010
-getBase(11, 8); // 13
-getBase(33, 11); // 30
+function findLongestZero(str) {
+  var longestGap = 0;
+  var currentRun = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "0") {
+      currentRun++;
+      if (currentRun > longestGap) {
+        longestGap = currentRun;
+      }
+    } else {
+      currentRun = 0;
+    }
+  }
+  return longestGap;
+}
+
+getBase(10, 2); // 1
+getBase(11, 8); // 0
+getBase(33, 2); // 4
